d89c9c868d2b1257f2609b1d7b329063a75eb640
Matozinho <matozinhof.academico@gmail.com>
2021-06-29 21:57:28 -0300

    implementing cesar's cypher in javascript
    Signed-off-by: Matozinho <matozinhof.academico@gmail.com>

---
+++ substituition cyphers/cesarsCypher/cesar.js
@@ -0,0 +1,26 @@
+const isLower = (character) => {
+  if (character === character.toLowerCase()) return true;
+  else return false;
+};
+
+const cesarsCypher = (text, n) => {
+  let cypheredText = [];
+
+  for (let i = 0; i < text.length; i++) {
+    let temp;
+
+    if (text[i] !== " ") {
+      if (isLower(text[i])) {
+        temp = ((text.charCodeAt(i) - 97 + n) % 26) + 97;
+        cypheredText.push(String.fromCharCode(temp));
+      } else {
+        temp = ((text.charCodeAt(i) - 65 + n) % 26) + 65;
+        cypheredText.push(String.fromCharCode(temp));
+      }
+    } else cypheredText.push(" ");
+  }
+
+  return cypheredText.join("");
+};
+
+console.log(cesarsCypher("FESTA CONFIRMADA NO PrOXIMO SABADO A NOiTe", 3));
