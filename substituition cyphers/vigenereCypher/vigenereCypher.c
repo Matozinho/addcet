1b972192c3e7ab00659a68f7ed80bd48eac003d3
Vinicius <viniciusmullerdefreitas309@gmail.com>
2021-07-07 23:13:59 -0300

    vigenere cypher inplementation in C with multiple calls
    Signed-off-by: Vinicius <viniciusmullerdefreitas309@gmail.com>

--- substituition cyphers/vigenereCypher/vigenereCypher.c
+++ substituition cyphers/vigenereCypher/vigenereCypher.c
@@ -1,11 +1,37 @@
+/*
+  Knowing the vigenere Cypher is a cesar Cypher with multiple keys
+  basend in another AudioWorkletNode, we can use a functio with multiple
+  calls for each key to make vigenereCypher work
+*/
+
 #include <stdio.h>
 #include <string.h>
 
 #define MaxSize 500
 
+    char cesar (char string, int n){
+
+        if(string >= 'a' && string <= 'z'){
+            string = string + n;
+            if(string > 'z'){
+                string -= 26;
+
+            }
+        }else if(string >= 'A' && string <= 'Z'){
+            string = string + n;
+            if(string > 'Z'){
+                string -= 26;
+
+            }
+        }
+
+        return string;
+    }
+
     int main(){
 
         char string[MaxSize], key[MaxSize], cypher[MaxSize];
+        char caractere;
         int size, size1;
         int i, j=0;
 
@@ -14,31 +40,32 @@
         printf("Digite a chave: ");
         scanf("%s", key);
 
-        size = strlen(string);
+        size  = strlen(string)-1;
         size1 = strlen(key);
 
-        for(i=0; i<size-1; i++){
+        for(int i=0; i<size; i++){
 
-            if(string[i] >= 'a' && string[i] <= 'z'){
-                cypher[i] = (string[i] - 97 + key[j] - 97) % 26 + 97;
+            caractere = string[i];
 
-            }else if(string[i] >= 'A' && string[i] <= 'Z'){
-                cypher[i] = (string[i] - 65 + key[j] - 65) % 26 + 65;
-            }
+            if(caractere != ' '){
 
-            if(string[i] != ' '){
-                if(j < size1-1){
-                    j++;
-                }else{
-                    j = 0;
+                if(key[j%size1] >= 'a' && key[j%size1] <= 'z'){
+                    cypher[i] = cesar(caractere, key[j%size1]-96);
+                
+                }else if(key[j%size1] >= 'A' && key[j%size1] <= 'Z'){
+                    cypher[i] = cesar(caractere, key[j%size1]-64);
                 }
+
+                j++;
+                
+            }else{
+                cypher[i] = ' ';
             }
 
         }
-
         cypher[size] = '\0';
 
-        printf("Palavra Cifrada : %s", cypher);
+        printf("Palavra Cifrada : %s\n", cypher);
 
         return 0;
     }
